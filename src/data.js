ac90258c751812c18a2ca6e9e49132c4845f1c9c
iMMoCodes <matti-makela@hotmail.com>
2021-10-12 19:10:59 +0300

    created announcement banner and slider

---
+++ src/data.js
@@ -0,0 +1,23 @@
+export const sliderItems = [
+  {
+    id: 1,
+    img: 'https://images.pexels.com/photos/291762/pexels-photo-291762.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    title: 'TEST TITLE 1',
+    desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus, officia.',
+    bg: 'f5fafd',
+  },
+  {
+    id: 2,
+    img: 'https://images.pexels.com/photos/318236/pexels-photo-318236.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    title: 'TEST TITLE 2',
+    desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus, officia.',
+    bg: 'fcf1ed',
+  },
+  {
+    id: 3,
+    img: 'https://images.pexels.com/photos/974911/pexels-photo-974911.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    title: 'TEST TITLE 3',
+    desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus, officia.',
+    bg: 'fbf0f4',
+  },
+]
