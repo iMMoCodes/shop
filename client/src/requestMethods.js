dfec70d018d3ae8fef35ffc27415ce66bca4f51d
iMMoCodes <matti-makela@hotmail.com>
2021-10-20 00:36:05 +0300

    -Added cookie-js package -Request methods now take token from cookies instead of local storage

--- client/src/requestMethods.js
+++ client/src/requestMethods.js
@@ -1,8 +1,8 @@
 import axios from 'axios'
+import Cookies from 'js-cookie'
 
 const BASE_URL = 'http://localhost:5000/api/v1/'
-const TOKEN = JSON.parse(JSON.parse(localStorage.getItem('persist:root')).user)
-  .currentUser?.accessToken
+const TOKEN = Cookies.get('jwt')
 
 export const publicRequest = axios.create({
   baseURL: BASE_URL,
