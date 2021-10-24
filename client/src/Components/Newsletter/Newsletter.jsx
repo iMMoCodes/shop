d834ee0a610a8bd91d38944fc51e2ac1b8de2c92
iMMoCodes <matti-makela@hotmail.com>
2021-10-24 20:38:30 +0300

    Uncommented newsletter and added alert when sent

--- client/src/Components/Newsletter/Newsletter.jsx
+++ client/src/Components/Newsletter/Newsletter.jsx
@@ -1,6 +1,8 @@
-// import { useState } from 'react'
-// import { publicRequest } from '../../requestMethods'
-import { Send } from '@material-ui/icons'
+import { useState, forwardRef } from 'react';
+import { publicRequest } from '../../requestMethods';
+import { Send } from '@material-ui/icons';
+import MuiAlert from '@mui/material/Alert';
+import { Snackbar } from '@mui/material';
 import {
   Container,
   Title,
@@ -8,19 +10,32 @@ import {
   InputContainer,
   Input,
   Button,
-} from './NewsletterStyles'
+} from './NewsletterStyles';
 
 const Newsletter = () => {
-  // const [email, setEmail] = useState('')
+  const [email, setEmail] = useState('');
+  const [open, setOpen] = useState(false);
 
-  // const handleClick = async (e) => {
-  //   e.preventDefault()
-  //   try {
-  //     await publicRequest.post('/users/newsletter', { email })
-  //   } catch (err) {
-  //     console.log(err)
-  //   }
-  // }
+  const handleClick = async (e) => {
+    e.preventDefault();
+    try {
+      await publicRequest.post('/users/newsletter', { email });
+      setOpen(true);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const Alert = forwardRef(function Alert(props, ref) {
+    return <MuiAlert elevation={6} ref={ref} variant='filled' {...props} />;
+  });
+
+  const handleClose = (event, reason) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+    setOpen(false);
+  };
 
   return (
     <Container>
@@ -33,15 +48,23 @@ const Newsletter = () => {
           type='email'
           name='email'
           placeholder='example@email.com'
-          // onChange={(e) => setEmail(e.target.value)}
+          onChange={(e) => setEmail(e.target.value)}
         />
         <Button>
-          {/* <Send onClick={handleClick} /> */}
-          <Send />
+          <Send onClick={handleClick} />
         </Button>
+        <Snackbar open={open} autoHideDuration={4000} onClose={handleClose}>
+          <Alert
+            onClose={handleClose}
+            severity='success'
+            sx={{ width: '100%' }}
+          >
+            Newsletter sent!
+          </Alert>
+        </Snackbar>
       </InputContainer>
     </Container>
-  )
-}
+  );
+};
 
-export default Newsletter
+export default Newsletter;
--- client/src/Components/Newsletter/NewsletterStyles.js
+++ client/src/Components/Newsletter/NewsletterStyles.js
@@ -1,5 +1,5 @@
-import styled from 'styled-components'
-import { mobile, tablet } from '../../responsive'
+import styled from 'styled-components';
+import { mobile, tablet } from '../../responsive';
 
 export const Container = styled.div`
   height: 60vh;
@@ -10,18 +10,18 @@ export const Container = styled.div`
   flex-direction: column;
   ${tablet({ height: '40vh' })}
   ${mobile({ height: '100%' })}
-`
+`;
 export const Title = styled.h1`
   font-size: 70px;
   margin-bottom: 20px;
   ${mobile({ textAlign: 'center', fontSize: '55px' })}
-`
+`;
 export const Description = styled.p`
   font-size: 24px;
   font-weight: 300;
   margin-bottom: 20px;
   ${tablet({ textAlign: 'center' })}
-`
+`;
 export const InputContainer = styled.div`
   width: 50%;
   height: 40px;
@@ -31,17 +31,18 @@ export const InputContainer = styled.div`
   align-items: center;
   border: 1px solid lightgray;
   ${mobile({ border: 'none', width: '80%' })}
-`
+`;
 export const Input = styled.input`
   border: none;
   flex: 8;
   padding-left: 20px;
   height: 100%;
-`
+`;
 export const Button = styled.button`
   flex: 1;
   border: none;
   background-color: teal;
   color: white;
   height: 100%;
-`
+  cursor: pointer;
+`;
